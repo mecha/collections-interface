c9f295f5c589258217099fe000c5497568c2fdd4
Anton Ukhanev <xedin.unknown@gmail.com>
2016-10-16 17:57:23 +0200

    Added `unsMany()`

--- src/UnsetCapableCollectionInterface.php
+++ src/UnsetCapableCollectionInterface.php
@@ -19,4 +19,17 @@ interface UnsetCapableCollectionInterface extends CollectionInterface
      * @return bool True if item has been removed; false otherwise.
      */
     public function uns($key);
+
+    /**
+     * Removes items with the specified keys.
+     *
+     * If removal of one of the keys fails, continues onto the next.
+     *
+     * @param string[]|\Traversable $keys The keys of items to remove.
+     *
+     * @since [*next-version*]
+     *
+     * @return bool False if one of the items has not been removed; true otherwise.
+     */
+    public function unsMany($keys);
 }
