c9eeb86d53ba39c06d01cafe77bef774eedecab4
Anton Ukhanev <xedin.unknown@gmail.com>
2016-10-18 18:42:57 +0200

    Added `SetInterface`

---
+++ src/SetInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dhii\Collection;
+
+/**
+ * A set, items for which can be added, removed, retrieved, and checked.
+ *
+ * @since [*next-version*]
+ */
+interface SetInterface extends
+    WritableSetInterface,
+    ReadableSetInterface,
+    RemovalCapableCollectionInterface
+{
+}
