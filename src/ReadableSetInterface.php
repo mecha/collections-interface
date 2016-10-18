7abf5d8ba4f10414b8685bf60e5316ebe1c77b0d
Anton Ukhanev <xedin.unknown@gmail.com>
2016-10-18 18:28:37 +0200

    Added `ReadableSetInterface`

---
+++ src/ReadableSetInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Collection;
+
+/**
+ * A set, items for which can be checked and retrieved, but not added or removed.
+ *
+ * @since [*next-version*]
+ */
+interface ReadableSetInterface extends BulkRetrievableCollectionInterface
+{
+}
