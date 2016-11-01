345c25b86279605478c19d301254677653814c88
Anton Ukhanev <xedin.unknown@gmail.com>
2016-11-01 13:21:51 +0100

    Added interface `TraversableCollectionInterface`

---
+++ src/TraversableCollectionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dhii\Collection;
+
+/**
+ * A collection, the items of which can be accessed and iterated over.
+ *
+ * @since [*next-version*]
+ */
+interface TraversableCollectionInterface extends
+    ReadableCollectionInterface,
+    IterableCollectionInterface
+{
+
+}
