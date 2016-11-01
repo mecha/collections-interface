da89e5b0c1ae8a8c4cad85a6386d2144014f80a9
Anton Ukhanev <xedin.unknown@gmail.com>
2016-11-01 13:17:04 +0100

    Added `WritableCollectionInterface`

---
+++ src/WritableCollectionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dhii\Collection;
+
+/**
+ * A collection, the item set of which may be manipulated indirectly.
+ *
+ * @since [*next-version*]
+ */
+interface WritableCollectionInterface extends
+    ExtensibleCollectionInterface,
+    ReducibleCollectionInterface
+{
+
+}
