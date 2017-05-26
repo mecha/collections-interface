1508aa1534f36742832ce17ec207c355d62f1745
Anton Ukhanev <xedin.unknown@gmail.com>
2017-05-26 16:24:30 +0200

    Base collection interface no longer requires `validate()`

--- src/BaseCollectionInterface.php
+++ src/BaseCollectionInterface.php
@@ -19,15 +19,4 @@ interface BaseCollectionInterface
      * @return bool True if the given item exists in this instance; false otherwise.
      */
     public function contains($item);
-
-    /**
-     * Determines whether the given item is a valid member of this collection.
-     *
-     * This is compatible with {@link https://github.com/Dhii/validator-interface/blob/master/src/ValidatorInterface.php ValidatorInterface}.
-     *
-     * @since [*next-version*]
-     *
-     * @param mixed $item The item to validate.
-     */
-    public function validate($item);
 }
